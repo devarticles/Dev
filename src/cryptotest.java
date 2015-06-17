1444db4872b23bb51bcedbfe96e1b59dbd8ab518
powerok <powerok@gmail.com>
2015-06-17 10:49:43 +0900

    add test Source

---
+++ src/cryptotest.java
@@ -0,0 +1,40 @@
+
+import java.security.*;
+ 
+public class cryptotest {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        MessageDigest md;
+        String message = "password";
+        try {
+              md= MessageDigest.getInstance("SHA-512"); // 128
+            
+//            md= MessageDigest.getInstance("SHA-256"); //64
+                  
+//            md= MessageDigest.getInstance("MD5");  //32
+//            md= MessageDigest.getInstance("SHA"); //40
+//            md= MessageDigest.getInstance("SHA-1");  //40
+//            md= MessageDigest.getInstance("SHA-384"); //96
+
+            
+            
+ 
+            md.update(message.getBytes());
+            byte[] mb = md.digest();
+            String out = "";
+            for (int i = 0; i < mb.length; i++) {
+                byte temp = mb[i];
+                String s = Integer.toHexString(new Byte(temp));
+                while (s.length() < 2) {
+                    s = "0" + s;
+                }
+                s = s.substring(s.length() - 2);
+                out += s;
+            }
+            System.out.println(out.length());
+            System.out.println("CRYPTO: " + out);
+ 
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
+    }
+}
